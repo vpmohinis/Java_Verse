8fe21f455f91554490f4a509511940b2eebf9b56
Mohini Sawant <mohinibusiness98@gmail.com>
2025-08-14 16:18:12 +0530

    Polymorphism

--- src/OOPS/SaticPolyMorph.java
+++ src/OOPS/SaticPolyMorph.java
@@ -6,12 +6,20 @@ class Mom
     {
         return("Mom calling"+a);//return type is string so need to define method as a string
     }
-
+}
+class Dad
+{
+    public String mother(int a, int b)
+    {
+        return("Dad calling : "+(a+b));
+    }
 }
 public class SaticPolyMorph
 {
     public static void main(String[] args) {
         Mom m=new Mom();
         System.out.println(m.mother(10));
+        Dad d=new Dad();
+        System.out.println(d.mother(10,11));
     }
 }
